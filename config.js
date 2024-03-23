3220d965a317aa7bea2e552d97d9513e794e4c7e
mrusama12 <111014940+mrusama12@users.noreply.github.com>
2024-03-23 13:02:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.antitags = true;
 global.autoTyping = false;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
-global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
-global.author = "SACHITH";                               //Do not change.
+global.Owner = ["+923225404375", "+923225404375"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923225404375", "+923225404375"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+923225404375"];
+global.OwnerName = "M⃜R⃜ U⃜S⃜A⃜M⃜A⃜ ꧁";
+global.BotName = "*☛⃟⚉ ☠₀₃ᷟ₀ᷢ₆₆ᷴ₂ᷤ₀ᷲ₃ᷟ₇ᷧ₆₃☠⚉⃟☚*";
+global.packname = "*☛⃟⚉ ☠₀₃ᷟ₀ᷢ₆₆ᷴ₂ᷤ₀ᷲ₃ᷟ₇ᷧ₆₃☠⚉⃟☚*";                             //Do not change.
+global.author = "𖣘ᴰᵃʳᴋ᭄ᵁˢᵃᵐᵃ🐲࿐";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
@@ -71,7 +71,7 @@ global.nttoxic = []
 global.ntnsfw = []
 global.ntvirtex = []
 global.rkyt = []
-global.wlcm = []
+global.wlcm = [true]
 global.gcrevoke = []
 global.autorep = []
 global.ntilink = []
